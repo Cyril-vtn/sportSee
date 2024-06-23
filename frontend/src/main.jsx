2f59cfefd96805f408c1b7eeeb6cb5738b9c5fff
Cyril-vtn <votion.cyril@gmail.com>
2024-06-23 23:29:25 +0200

    chore: Update main.jsx to use Home component as the index route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import { Layout } from "./Layout";
+import { Home } from "./pages/Home";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -17,7 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
             </Layout>
           }
         >
-          {/* <Route index element={<App />} /> */}
+          <Route index element={<Home />} />
         </Route>
       </Routes>
     </BrowserRouter>
