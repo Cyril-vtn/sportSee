078cedbceae38898b2ce76b9e4a74f42da871f25
Cyril-vtn <votion.cyril@gmail.com>
2024-06-30 23:47:07 +0200

    feat: Add CustomRadialBarChart component for displaying radial bar charts

---
+++ frontend/src/components/CustomRadialBarChart.jsx
@@ -0,0 +1,47 @@
+import {
+  PolarAngleAxis,
+  RadialBar,
+  RadialBarChart,
+  ResponsiveContainer,
+} from "recharts";
+import "../styles/customRadialBarChart.css";
+
+export const CustomRadialBarChart = ({ score }) => {
+  const scoreData = [{ kind: "Score", value: score * 100 }];
+
+  console.log(scoreData);
+
+  return (
+    scoreData && (
+      <div className="customRadialBarChart">
+        <div className="customRadialBarChart_title">
+          <h3>Score</h3>
+        </div>
+        <ResponsiveContainer width="100%" height="100%">
+          <RadialBarChart
+            innerRadius="65%"
+            outerRadius="78%"
+            data={scoreData}
+            startAngle={-270}
+            endAngle={90}
+          >
+            <PolarAngleAxis
+              type="number"
+              domain={[0, 100]}
+              angleAxisId={0}
+              tick={false}
+            />
+            <RadialBar dataKey="value" cornerRadius={10} fill="#FF0101" />
+          </RadialBarChart>
+        </ResponsiveContainer>
+        <div className="customRadialBarChart_score">
+          <p>{score && score * 100}%</p>
+          <h3 className="customRadialBarChart_score_title">
+            de votre <br /> objectif
+          </h3>
+          <div className="customRadialBarChart_score_circle" />
+        </div>
+      </div>
+    )
+  );
+};
